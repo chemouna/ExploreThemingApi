3ab8f0a5543ce41ab079d9538cdfdc70c816ac36
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-09-25 17:20:00 +0100

    Add javadocs.

--- src/com/mounacheikhna/Themable.java
+++ src/com/mounacheikhna/Themable.java
@@ -6,6 +6,10 @@ public interface Themable {
 
     List<Themable> getThemableChildren();
 
+    /**
+     * this visit is necessary just to dispatch to children of a component so that they theme themeselves
+     * @param themeRenderer
+     */
     void visit(ThemeRenderer themeRenderer);
 
 }
